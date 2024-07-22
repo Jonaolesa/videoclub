144b6553e0cb57aceeb63ea67a1d27d7b024f3fd
Jonathan Cristian <jona_10ts@hotmail.com>
2024-07-22 18:50:36 +0200

    refactor: Creacion de interfaz Model y de DAO_CSV como clase concreta y con model como atributo de clase

--- app/modelos.py
+++ app/modelos.py
@@ -1,7 +1,17 @@
 from abc import ABC, abstractmethod
 import csv
 
-class Director:
+class Model(ABC):
+    @classmethod
+    @abstractmethod
+    def create_from_dict(cls, diccionario):
+        pass
+
+class Director(Model):
+    @classmethod
+    def create_from_dict(cls, diccionario):
+        return cls(diccionario["nombre"], int(diccionario["id"]))
+
     def __init__(self, nombre: str, id: int = -1):
         self.nombre = nombre
         self.id = id
@@ -18,7 +28,14 @@ class Director:
         return hash((self.id, self.nombre))
 
 
-class Pelicula:
+class Pelicula(Model):
+    @classmethod
+    def create_from_dict(cls, diccionario):
+        return cls(diccionario["titulo"], 
+                   diccionario["sinopsis"], 
+                   int(diccionario["director_id"]), 
+                   int(diccionario["id"]))
+    
     def __init__(self, titulo: str, sinopsis: str, director: object, id = -1):
         self.titulo = titulo
         self.sinopsis = sinopsis
@@ -39,6 +56,17 @@ class Pelicula:
             self._id_director = value
         else:
             raise TypeError(f"{value} debe ser un entero o instancia de Director")
+
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return self.titulo == other.titulo and self.sinopsis == other.sinopsis and self.director == other.director and self.id == other.id
+        return False
+    
+    def __hash__(self):
+        return hash((self.id, self.titulo, self.sinopsis, self.director))
+    
+    def __repr__(self):
+        return f"Pelicula ({self.id}): {self.titulo}, {self.director}"
 
 
 class DAO(ABC):
@@ -64,30 +92,23 @@ class DAO(ABC):
     def todos(self):
         pass
 
-class DAO_CSV_Director(DAO):
+class DAO_CSV(DAO):
+    model = None
+
     def __init__(self, path):
         self.path = path
 
     def todos(self):
-        with open(self.path, "r", newline="") as fichero:
+        with open(self.path, "r", newline="", encoding="utf-8") as fichero:
             lector_csv = csv.DictReader(fichero, delimiter=";", quotechar="'")
             lista = []
             for registro in lector_csv:
-                lista.append(Director(registro["nombre"], int(registro["id"])))
-        return lista        
+                lista.append(self.model.create_from_dict(registro))
+        return lista     
 
-class DAO_CSV_Pelicula(DAO):
-    def __init__(self, path: str):
-        self.path = path
+class DAO_CSV_Director(DAO_CSV):
+    model = Director
 
-    def todos(self):
-        with open(self.path, "r", newline="") as fichero:
-            lector_csv = csv.DictReader(fichero, delimiter=";", quotechar="'")
-            lista = []
-            for registro in lector_csv:
-                lista.append(Pelicula(registro["titulo"], 
-                                      registro["sinopsis"], 
-                                      int(registro["director_id"]), 
-                                      int(registro["id"])))
-                
-        return lista
+class DAO_CSV_Pelicula(DAO_CSV):
+    model = Pelicula
+
